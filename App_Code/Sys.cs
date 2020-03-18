b71c444dfa5a8e508448fcb18e1dcbbbd02924c6
IreneGigabit <33124327+IreneGigabit@users.noreply.github.com>
2020-03-18 17:54:50 +0800

    Update Sys.cs

--- App_Code/Sys.cs
+++ App_Code/Sys.cs
@@ -1,108 +1,173 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
 using System.Web;
-
-/// <summary>
-/// 連線字串設定
-/// </summary>
-public static class Conn
+using System.Data.SqlClient;
+using System.Collections.Generic;
+public class Sys
 {
-    private static string Host = HttpContext.Current.Request.ServerVariables["HTTP_HOST"].ToString().ToUpper();
+	public static string Host = HttpContext.Current.Request.ServerVariables["HTTP_HOST"].ToString().ToLower();
+	/// <summary>  
+	/// 取得某個Session值  
+	/// </summary>  
+	/// <param name="strSessionName">Session對象名稱</param>  
+	/// <returns>Session值</returns>  
+	public static string GetSession(string strSessionName) {
+		return (HttpContext.Current.Session[strSessionName] ?? "").ToString();
+	}
 
-    /// <summary>
-    /// 爭救案系統
-    /// </summary>
-    public static string OptK {
-        get {
-            switch (Host) {
-                case "SIK10": return Sys.getConnString("prod_optk");//正式環境
-                case "WEB10": return Sys.getConnString("test_optk");//使用者測試環境
-                default: return Sys.getConnString("dev_optk");//開發環境
-            }
-        }
-    }
+	/// <summary>  
+	/// 取得Session ID
+	/// </summary>  
+	/// <returns>Session值</returns>  
+	public static string GetSessionID() {
+		return HttpContext.Current.Session.SessionID;
+	}
+
+	/// <summary>  
+	/// 設定Session值
+	/// </summary>  
+	/// <returns>Session值</returns>  
+	public static void SetSession(string strSessionName, object sessionValue) {
+		HttpContext.Current.Session[strSessionName] = sessionValue;
+	}
+
+	/// <summary>  
+	/// 取得應用程式在伺服器上虛擬應用程式根路徑ex:/NOpt
+	/// </summary>  
+	/// <returns>應用程式根路徑</returns>  
+	public static string GetRootDir() {
+		return HttpContext.Current.Request.ApplicationPath;
+	}
+
+	public static bool IsAdmin() {
+		bool b = (GetSession("scode") == "admin" || GetSession("LoginGrp").ToLower().IndexOf("admin") > -1);
+		return b;
+	}
+
+	public static string getConnString(string parameter) {
+		string str = "";
+		try {
+			str = ConfigurationManager.ConnectionStrings[parameter].ConnectionString;
+		}
+		catch {
+		}
+		return str;
+	}
+
+	public static string getAppSetting(string parameter) {
+		string str = "";
+		try {
+			str = ConfigurationManager.AppSettings[parameter];
+		}
+		catch {
+		}
+		return str;
+	}
+
+	public static void errorLog(Exception ex, string sqlStr, string prgID) {
+		List<string> sqlList = new List<string>();
+		sqlList.Add(sqlStr);
+		errorLog(ex, sqlList, prgID);
+	}
+
+	public static void errorLog(Exception ex, List<string> sqlList, string prgID) {
+		using (SqlConnection cn = new SqlConnection(Conn.OptK)) {
+			cn.Open();
+			string eSQL = "INSERT INTO error_log(log_date, log_uid, syscode, prgid, MsgStr, SQLstr, StackStr) VALUES (";
+			eSQL = eSQL + "getdate(),";
+			eSQL = eSQL + "'" + (GetSession("scode") == "" ? GetSessionID() : GetSession("scode")) + "',";
+			eSQL = eSQL + "'" + (GetSession("Syscode") == "" ? GetRootDir().Replace("/", "") : GetSession("Syscode")) + "',";
+			eSQL = eSQL + "'" + prgID + "',";
+			eSQL = eSQL + "'" + ex.Message.Replace("'", "''") + "',";
+			eSQL = eSQL + "'" + string.Join("\r\n=====\r\n", sqlList.ToArray()).Replace("'", "''") + "',";
+			eSQL = eSQL + "'" + ex.StackTrace.Replace("'", "''") + "')";
+
+			SqlCommand cmd = new SqlCommand(eSQL, cn);
+			cmd.ExecuteNonQuery();
+		}
+	}
 
 	/// <summary>
-	/// 區所案件管理系統
+	/// 聖島人主機
 	/// </summary>
-	public static string OptB(string pBranch) {
+	public static string SIServer {
+		get {
+			if (Host == "web10") return "web01";
+			if (Host == "web08") return "web02";
+			switch (Host.Substring(0, 1)) {
+				case "w":
+				case "b":
+				case "l": return Host;
+				default: return "sin32";
+			}
+		}
+	}
+
+	/// <summary>
+	/// 案件資料庫名稱(抓共用設定用.ex:code_br,case_fee)
+	/// </summary>
+	public static string tdbname {
+		get {
+			switch (Host) {
+				case "sik10": return "sikdbs";//正式環境
+				default: return "sindbs";//開發環境
+			}
+		}
+	}
+
+	/// <summary>
+	/// 區所上傳檔案檢視主機(iis)
+	/// </summary>
+	public static string webservername(string pBranch) {
 		string rtnStr = "";
 		switch (Host) {
-			case "SIK10": //正式環境
-				if (pBranch.ToUpper() == "N") rtnStr = Sys.getConnString("prod_optBN");
-				if (pBranch.ToUpper() == "C") rtnStr = Sys.getConnString("prod_optBC");
-				if (pBranch.ToUpper() == "S") rtnStr = Sys.getConnString("prod_optBS");
-				if (pBranch.ToUpper() == "K") rtnStr = Sys.getConnString("prod_optBK");
+			case "sik10": //正式環境
+				if (pBranch.ToUpper() == "N") rtnStr = "sinn03";
+				if (pBranch.ToUpper() == "C") rtnStr = "sic09";
+				if (pBranch.ToUpper() == "S") rtnStr = "sis09";
+				if (pBranch.ToUpper() == "K") rtnStr = "sik09";
 				break;
-			case "WEB10":
-				rtnStr = Sys.getConnString("test_optBN");//測試環境
+			case "web10":
+				rtnStr = "web01";//測試環境
 				break;
 			default:
-				rtnStr = Sys.getConnString("dev_optBN");//開發環境
+				rtnStr = "web02";//開發環境
 				break;
 		}
 		return rtnStr;
 	}
 
-    /// <summary>
-    /// 案件管理系統(總)
-    /// </summary>
-    public static string OptBM {
-        get {
-            switch (Host) {
-                case "SIK10": return Sys.getConnString("prod_optBM");//正式環境
-				case "WEB10": return Sys.getConnString("test_optBM");//使用者測試環境
-                default: return Sys.getConnString("dev_optBM");//開發環境
-            }
-        }
-    }
-
 	/// <summary>
-	/// 帳款資料使用
+	/// 區所上傳檔案實體主機(fileServer)
 	/// </summary>
-	public static string Acc(string pBranch) {
+	public static string uploadservername(string pBranch) {
 		string rtnStr = "";
 		switch (Host) {
-			case "SIK10": //正式環境
-				if (pBranch.ToUpper() == "N") rtnStr = Sys.getConnString("prod_Nacc");
-				if (pBranch.ToUpper() == "C") rtnStr = Sys.getConnString("prod_Cacc");
-				if (pBranch.ToUpper() == "S") rtnStr = Sys.getConnString("prod_Sacc");
-				if (pBranch.ToUpper() == "K") rtnStr = Sys.getConnString("prod_Kacc");
+			case "sik10": //正式環境
+				if (pBranch.ToUpper() == "N") rtnStr = "sinn11";
+				if (pBranch.ToUpper() == "C") rtnStr = "sic11";
+				if (pBranch.ToUpper() == "S") rtnStr = "sis11";
+				if (pBranch.ToUpper() == "K") rtnStr = "sik08";
 				break;
-			case "WEB10":
-				rtnStr = Sys.getConnString("test_Nacc");//測試環境
+			case "web10":
+				rtnStr = "web01";//測試環境
 				break;
 			default:
-				rtnStr = Sys.getConnString("dev_Nacc");//開發環境
+				rtnStr = "web02";//開發環境
 				break;
 		}
 		return rtnStr;
 	}
 
-    /// <summary>
-    /// Sysctrl
-    /// </summary>
-    public static string Sysctrl {
-        get {
-            switch (Host) {
-                case "SIK10": return Sys.getConnString("prod_sysctrl");//正式環境
-				case "WEB10": return Sys.getConnString("test_sysctrl");//使用者測試環境
-                default: return Sys.getConnString("dev_sysctrl");//開發環境
-            }
-        }
-    }
+    /// <summary>  
+    /// 組本所編號
+    /// </summary>  
+    public static string formatSeq(string seq, string seq1, string country, string branch, string dept) {
+		string lseq = branch + dept + "-" + seq;
+		lseq += (seq1 != "_" ? ("-" + seq1) : "");
+		lseq += (country != "" ? (" " + country.ToUpper()) : "");
+		return lseq;
+	}
 
-    /// <summary>
-    /// ODBCDSN
-    /// </summary>
-    public static string ODBCDSN {
-        get {
-            switch (Host) {
-                case "SIK10": return Sys.getConnString("prod_sysctrl");//正式環境
-				case "WEB10": return Sys.getConnString("test_sysctrl");//使用者測試環境
-                default: return Sys.getConnString("dev_sysctrl");//開發環境
-            }
-        }
-    }
+
 }
