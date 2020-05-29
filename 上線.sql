fd04816332c51eeaf0c37bd89bbb5e526ebe275e
IreneGigabit <33124327+IreneGigabit@users.noreply.github.com>
2020-05-29 17:52:37 +0800

    Update 上線.sql

--- 上線.sql
+++ 上線.sql
@@ -12,19 +12,6 @@ ALTER TABLE dbo.case_opt SET (LOCK_ESCALATION = TABLE)
 GO
 COMMIT
 
-BEGIN TRANSACTION
-GO
-ALTER TABLE dbo.case_opt_log ADD
-	rectitle_name varchar(320) NULL,
-	send_way varchar(2) NULL,
-	receipt_type varchar(1) NULL,
-	receipt_title varchar(1) NULL
-GO
-ALTER TABLE dbo.case_opt_log SET (LOCK_ESCALATION = TABLE)
-GO
-COMMIT
-
-
 SELECT DISTINCT 'EXEC sp_refreshview ' + name + '' 
 FROM sys.objects so INNER JOIN sys.sql_dependencies sd 
 ON so.object_id = sd.object_id 
